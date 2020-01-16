0cf633f23add14781b69497ff6a9daf98098f198
jubaan <julio.ab@outlook.com>
2020-01-15 19:56:10 -0600

    UPDATE: bubble sort for numbers

--- advanced-building-blocks__bubble-sort.rb
+++ advanced-building-blocks__bubble-sort.rb
@@ -1,1 +1,13 @@
-# >>>  start writing your code in this line  <<<
+# Bubble sort method
+
+def bubble_sort(arr)
+  counter = 0
+  (arr.length**2).times do
+    arr[counter], arr[counter + 1] = arr[counter + 1], arr[counter] if arr[counter] > arr[counter + 1]
+    counter += 1
+    counter = 0 if counter == arr.length - 1
+  end
+  arr
+end
+
+bubble_sort([4, 3, 78, 2, 0, 2])
