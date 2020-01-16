0d5daecccf71ce7f942c53a8ebd7fa3366afdaee
jubaan <julio.ab@outlook.com>
2020-01-16 00:25:48 -0600

    fix: fIX: rubocop lint issues
    Erase whitetailing and doubled method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -11,15 +11,3 @@ def bubble_sort(arr)
 end
 
 bubble_sort([4, 3, 78, 2, 0, 2])
-
-def bubble_sort(arr)
-  counter = 0
-  (arr.length**2).times do
-    arr[counter], arr[counter + 1] = arr[counter + 1], arr[counter] if arr[counter] > arr[counter + 1]
-    counter += 1
-    counter = 0 if counter == arr.length - 1
-  end
-  arr
-end
-
-bubble_sort([4, 3, 78, 2, 0, 2])
