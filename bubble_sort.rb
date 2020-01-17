17c08a55f2a51c35f25f3bded59bb7bf359cb60d
jubaan <julio.ab@outlook.com>
2020-01-17 12:28:49 -0600

    FIX: Trailing whitespace in line 9

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -6,7 +6,7 @@ def bubble_sort(arr)
     swap = false
     (arr.length - 1).times do |i|
       if arr[i] > arr[i + 1]
-        arr[i], arr[i + 1] = arr[i + 1], arr[i] 
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
         swap = true
       end
     end
